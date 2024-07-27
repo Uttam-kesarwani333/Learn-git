576bd0683cce1d517d9f3a0de09c687d4adcdc2e
Uttam <Uttam@gmail.com>
2024-07-27 23:07:36 +0530

    Joining Third Complete

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // This is Script Files
-<<<<<<< HEAD
+
 
 // This is written in files by Master branch
-=======
+
 // This is written in files by Master branch
 // This is written in files by Master branch123
 // This is written in files by Master branch456
 // This is written in files by Master branch789
->>>>>>> DevA
